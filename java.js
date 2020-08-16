90de62a7ae7fc54a4604850df26641b98f5f5bc2
SumonWebDev <67514941+SumonWebDev@users.noreply.github.com>
2020-08-16 09:38:34 +0600

    file send

---
+++ java.js
@@ -0,0 +1,8 @@
+const numbers=[2,3,5,6,7];
+const output=[];
+for(var i=0;i<numbers.length;i++){
+    const element=numbers[i];
+    const result=element*element;
+    output.push(result);
+}
+console.log(output);
