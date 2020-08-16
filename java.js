ee8ed9f121844745ac5b89031bdc26d73b1dd14d
SumonWebDev <67514941+SumonWebDev@users.noreply.github.com>
2020-08-16 19:56:35 +0600

    file send

--- java.js
+++ java.js
@@ -1,4 +1,4 @@
-const numbers=[2,3,5,6,7];
+/*const numbers=[2,3,5,6,7];
 const output=[];
 for(var i=0;i<numbers.length;i++){
     const element=numbers[i];
@@ -6,3 +6,21 @@ for(var i=0;i<numbers.length;i++){
     output.push(result);
 }
 console.log(output);
+*/
+
+
+/*const students=[
+    {id:21,name:'omar sunny'},
+    {id:31,name:'sunny'},
+    {id:41,name:'omar'},
+    {id:71,name:'dipjol'},
+];
+const names=students.map(s=>s.name);
+console.log(names);
+const ids=students.map(s=>s.id);
+console.log(ids);
+const bigger=students.filter(s=>s.id>46);
+console.log(bigger);
+*/
+
+
